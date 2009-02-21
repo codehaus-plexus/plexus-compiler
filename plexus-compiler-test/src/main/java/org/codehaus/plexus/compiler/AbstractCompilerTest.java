31908f6161ac339ed48c610ff1a940b113464e60
bentmann <bentmann@30dfa635-7bf8-0310-9608-e41f2d776689>
2009-02-21 10:40:19 +0000

    o Updated to maven-artifact-test:2.0.10 to have tests recognize maven.repo.local

--- plexus-compiler-test/src/main/java/org/codehaus/plexus/compiler/AbstractCompilerTest.java
+++ plexus-compiler-test/src/main/java/org/codehaus/plexus/compiler/AbstractCompilerTest.java
@@ -27,16 +27,10 @@ package org.codehaus.plexus.compiler;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.DefaultArtifact;
 import org.apache.maven.artifact.handler.DefaultArtifactHandler;
-import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.DefaultArtifactRepository;
 import org.apache.maven.artifact.test.ArtifactTestCase;
 import org.apache.maven.artifact.versioning.VersionRange;
-import org.apache.maven.settings.Settings;
-import org.apache.maven.settings.io.xpp3.SettingsXpp3Reader;
 
 import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.ReaderFactory;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -240,44 +234,5 @@ public abstract class AbstractCompilerTest
 
         return getLocalArtifactPath( artifact );
     }
-
-// TODO: Temporarily duplicated code from maven-artifact-test:2.0.10 until released to fix lookup of local repo
-
-    private ArtifactRepository localRepository;
-
-    protected File getLocalArtifactPath( Artifact artifact )
-    {
-        return new File( localRepository.getBasedir(), localRepository.pathOf( artifact ) );
-    }
-
-    protected void setUp()
-        throws Exception
-    {
-        super.setUp();
-
-        String localRepo = System.getProperty( "maven.repo.local" );
-
-        if ( localRepo == null )
-        {
-            File settingsFile = new File( System.getProperty( "user.home" ), ".m2/settings.xml" );
-            if ( settingsFile.exists() )
-            {
-                Settings settings = new SettingsXpp3Reader().read( ReaderFactory.newXmlReader( settingsFile ) );
-                localRepo = settings.getLocalRepository();
-            }
-        }
-
-        if ( localRepo == null )
-        {
-            localRepo = System.getProperty( "user.home" ) + "/.m2/repository";
-        }
-
-        ArtifactRepositoryLayout repositoryLayout = (ArtifactRepositoryLayout) container.lookup(
-            ArtifactRepositoryLayout.ROLE, "default" );
-
-        localRepository = new DefaultArtifactRepository( "local", "file://" + localRepo, repositoryLayout );
-    }
-
-// END DUPLICATED CODE
 
 }
