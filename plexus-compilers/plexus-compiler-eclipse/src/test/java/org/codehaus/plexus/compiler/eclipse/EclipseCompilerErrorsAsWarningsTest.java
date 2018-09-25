91eee8fde501583692aa5b27124c0fbfe7bd2e70
Jason Faust <jason.faust@gmail.com>
2018-09-24 22:43:36 -0400

    Refactored Eclipse Compiler tests
    * ECJ updated to 3.14.0
    * Change of IllegalArgumentException into the ECJ Exception

--- plexus-compilers/plexus-compiler-eclipse/src/test/java/org/codehaus/plexus/compiler/eclipse/EclipseCompilerErrorsAsWarningsTest.java
+++ plexus-compilers/plexus-compiler-eclipse/src/test/java/org/codehaus/plexus/compiler/eclipse/EclipseCompilerErrorsAsWarningsTest.java
@@ -1,52 +1,20 @@
 package org.codehaus.plexus.compiler.eclipse;
 
-import org.codehaus.plexus.compiler.AbstractCompilerTest;
 import org.codehaus.plexus.compiler.CompilerConfiguration;
 
-import java.util.Arrays;
-import java.util.Collection;
+/**
+ * @author <a href="jason.faust@gmail.com">Jason Faust</a>
+ */
+public class EclipseCompilerErrorsAsWarningsTest extends AbstractEclipseCompilerTest {
 
-public class EclipseCompilerErrorsAsWarningsTest extends AbstractCompilerTest
-{
-
-    protected void configureCompilerConfig( CompilerConfiguration compilerConfig )
-    {
-        compilerConfig.addCompilerCustomArgument( "-errorsAsWarnings", "true" );
+    public EclipseCompilerErrorsAsWarningsTest() {
+        super(0, 6, 1);
     }
 
-    public void setUp() throws Exception
-    {
-        super.setUp();
-
-        setCompilerDebug( true );
-        setCompilerDeprecationWarnings( true );
+    @Override
+    protected void configureCompilerConfig(CompilerConfiguration compilerConfig) {
+        super.configureCompilerConfig(compilerConfig);
+        compilerConfig.addCompilerCustomArgument("-errorsAsWarnings", "true");
     }
 
-    protected String getRoleHint()
-    {
-        return "eclipse";
-    }
-
-    protected int expectedErrors()
-    {
-        return 0;
-    }
-
-    protected int expectedWarnings()
-    {
-        return 6;
-    }
-
-    protected Collection<String> expectedOutputFiles()
-    {
-        return Arrays.asList( new String[] {
-            "org/codehaus/foo/Deprecation.class",
-            "org/codehaus/foo/ExternalDeps.class",
-            "org/codehaus/foo/Person.class",
-            "org/codehaus/foo/ReservedWord.class",
-            //"org/codehaus/foo/Bad.class",             // This one has no class file generated as it's one big issue
-            //"org/codehaus/foo/UnknownSymbol.class",
-            //"org/codehaus/foo/RightClassname.class"
-        });
-    }
 }
