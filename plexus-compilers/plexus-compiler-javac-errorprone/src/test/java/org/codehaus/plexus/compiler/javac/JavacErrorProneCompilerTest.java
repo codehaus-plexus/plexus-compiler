477bb49dae92265a42a59633ae5305ea9be1787b
Sylwester Lachiewicz <slachiewicz@apache.org>
2023-03-24 18:59:58 +0100

    Restore Java 8 compatibility
    aspectj 1.9.8+ requires Java 11
    https://github.com/eclipse/org.aspectj/blob/master/docs/dist/doc/README-199.html
    
    org.eclipse.jdt:ecj 3.27.0+ requires Java 11
    
    com.google.errorprone:error_prone_core 2.9.0+ requires Java 11

--- plexus-compilers/plexus-compiler-javac-errorprone/src/test/java/org/codehaus/plexus/compiler/javac/JavacErrorProneCompilerTest.java
+++ plexus-compilers/plexus-compiler-javac-errorprone/src/test/java/org/codehaus/plexus/compiler/javac/JavacErrorProneCompilerTest.java
@@ -9,10 +9,6 @@ public class JavacErrorProneCompilerTest
     extends AbstractCompilerTest
 {
 
-    protected boolean java8() {
-        return System.getProperty( "java.version" ).startsWith( "1.8" );
-    }
-
     @Override
     protected String getRoleHint()
     {
@@ -22,11 +18,13 @@ public class JavacErrorProneCompilerTest
     @Override
     protected int expectedWarnings()
     {
-        if ( java8() ) {
+        String javaVersion = getJavaVersion();
+        if (javaVersion.startsWith("1.8")) {
             return 1;
-        }
-        else
-        {
+        } else {
+            if (javaVersion.contains("18") || javaVersion.contains("19") || javaVersion.contains("20")) {
+                return 5;
+            }
             return 2;
         }
     }
