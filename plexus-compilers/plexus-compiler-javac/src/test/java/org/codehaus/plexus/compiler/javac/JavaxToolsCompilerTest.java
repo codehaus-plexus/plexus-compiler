459848fff09af3d22e15a4718f6213d577cdeb12
Sylwester Lachiewicz <slachiewicz@apache.org>
2024-08-06 02:20:01 +0200

    Fix for Java 21, 22

--- plexus-compilers/plexus-compiler-javac/src/test/java/org/codehaus/plexus/compiler/javac/JavaxToolsCompilerTest.java
+++ plexus-compilers/plexus-compiler-javac/src/test/java/org/codehaus/plexus/compiler/javac/JavaxToolsCompilerTest.java
@@ -27,9 +27,7 @@ public class JavaxToolsCompilerTest extends AbstractJavacCompilerTest {
     @Override
     protected int expectedWarnings() {
         String javaVersion = getJavaVersion();
-        if (javaVersion.contains("21") || javaVersion.contains("22")) {
-            return 8;
-        } else if (javaVersion.contains("23")) {
+        if (javaVersion.contains("21") || javaVersion.contains("22") || javaVersion.contains("23")) {
             return 1;
         } else {
             return super.expectedWarnings();
